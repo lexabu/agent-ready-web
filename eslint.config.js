22d36b09652d58033a3d325b96edd9f9f6453489
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2026-04-21 06:37:15 -0500

    chore: root TS/ESLint/Prettier/Vitest-workspace config

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: [
+      '**/dist/**',
+      '**/.next/**',
+      '**/node_modules/**',
+      '**/coverage/**',
+      '**/.wrangler/**',
+    ],
+  },
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+);
---
+++ vitest.workspace.ts
@@ -0,0 +1,7 @@
+export default [
+  'packages/core',
+  'packages/next',
+  'packages/cloudflare',
+  'examples/nextjs',
+  'examples/cloudflare-worker',
+];
